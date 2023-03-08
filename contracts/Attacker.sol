8e2ff3c0e1df6badb587a0a59edf98e697e5508d
Kristian Apostolov <kristianapostolovcontacts@gmail.com>
2023-03-08 20:21:13 +0200

    The abstract Attacker contract

---
+++ contracts/Attacker.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IAttacker {
+    receive() external payable;
+    function startAttack() external;
+}
+
+abstract contract Attacker is IAttacker, Ownable {
+
+    error NotImplemented();
+
+    receive() external payable {}
+    
+    function startAttack() external virtual {
+        revert NotImplemented();
+    }
+
+}
